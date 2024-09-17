6fdcc272edeaf8fbd939ad8aa1012f543575ff3f
Michael Bunte <michaelwbunte@gmail.com>
2024-09-17 16:34:58 -0700

    Process streamlined

--- create_backend_tables.py
+++ create_backend_tables.py
@@ -2,6 +2,8 @@ import psycopg2
 from psycopg2 import sql
 import subprocess
 import create_plc_table_commands
+from datetime import datetime, timedelta
+
 '''
 Connection Strings
 psql -h localhost -p 5432 -U michaelbunte -d waterexp
@@ -93,34 +95,37 @@ def createWaterSystemTable(systemName, cursor):
 
 
 def main():
-    print("Removing previously downloaded database")
-    run('rm -rf ./data')
+    # print("Removing previously downloaded database")
+    # run('rm -rf ./data')
 
-    run('mkdir data')
-    print("Downloading remote historical postgres data")
-    run('pg_dump -U read_only -h postgres.svwaternet.org -p 5432 -t bluerock_plc_values -t pryor_farm_plc_values -t santa_teresa_plc_values tsdb > ./data/dump.sql', printerr=False)
+    # run('mkdir data')
+    # print("Downloading remote historical postgres data")
+    # run('pg_dump -U read_only -h postgres.svwaternet.org -p 5432 -t bluerock_plc_values -t pryor_farm_plc_values -t santa_teresa_plc_values tsdb > ./data/dump.sql', printerr=False)
 
-    print("PSQL Dump Size:")
-    run('cat ./data/dump.sql | wc -c', printout=True)
+    # print("PSQL Dump Size:")
+    # run('cat ./data/dump.sql | wc -c', printout=True)
 
-    print("Killing any process running at port 5432")
-    run("kill -9 $(lsof -n -i :5432 | tail -n +2 | head -n 1 | awk '{print $2}')")
+    # print("Killing any process running at port 5432")
+    # run("kill -9 $(lsof -n -i :5432 | tail -n +2 | head -n 1 | awk '{print $2}')")
 
-    print("Removing any previous cluster data")
-    run("rm -rf ./cluster")
+    # print("Removing any previous cluster data")
+    # run("rm -rf ./cluster")
 
-    print("Initializing new database")
-    start_command = run("pg_ctl -D ./cluster initdb 2>/dev/null | tail -n 2").stdout
-    print(start_command)
-    print("Running new database")
-    run(start_command)
+    # print("Initializing new database")
+    # start_command = run("pg_ctl -D ./cluster initdb 2>/dev/null | tail -n 2").stdout
+    # print(start_command)
+    # print("Running new database")
+    # run(start_command)
 
-    conn = psycopg2.connect(database="postgres")
-    conn.autocommit = True 
-    cursor = conn.cursor()
-    cursor.execute(sql.SQL("CREATE DATABASE waterexp;"))
-    cursor.close()
-    conn.close()
+    # conn = psycopg2.connect(database="postgres")
+    # conn.autocommit = True 
+    # cursor = conn.cursor()
+    # cursor.execute(sql.SQL("CREATE DATABASE waterexp;"))
+    # cursor.close()
+    # conn.close()
+
+    run("dropdb waterexp || {}")
+    run("createdb waterexp")
 
     conn = psycopg2.connect(database="waterexp")
     conn.autocommit = True 
@@ -141,7 +146,7 @@ def main():
     createWaterSystemTable("bluerock", cursor)
 
     print("Creating Santa Teresa Tables")
-    createWaterSystemTable("santa_terersa", cursor)
+    createWaterSystemTable("santa_teresa", cursor)
 
     print("Creating Pryor Farms Tables")
     createWaterSystemTable("pryor_farms", cursor)
@@ -155,14 +160,17 @@ def create_plc_tables():
     conn.autocommit = True 
     cursor = conn.cursor()
     cursor.execute(sql.SQL(create_plc_table_commands.createBluerock("bluerock_plc_values")))
-    # cursor.execute(sql.SQL(create_plc_table_commands.createSantaTeresa("santa_teresa_plc_values")))
-    # cursor.execute(sql.SQL(create_plc_table_commands.createSantaTeresa("pryor_farms_plc_values")))
+    cursor.execute(sql.SQL(create_plc_table_commands.createSantaTeresa("santa_teresa_plc_values")))
+    cursor.execute(sql.SQL(create_plc_table_commands.createPryorFarms("pryor_farms_plc_values")))
     conn.close()
 
 
 # bluerock size: 20630192
-def remote_connect():
-    
+def remote_connect(
+        local_db_name,
+        remote_db_name,
+        db_row_count
+):
     remote_conn = psycopg2.connect(
         dbname="tsdb",
         host="postgres.svwaternet.org",
@@ -176,36 +184,127 @@ def remote_connect():
     local_cursor = local_conn.cursor()
     
     offset = 0
-    limit = 10 ** 6
+    limit = 10 ** 2
     while True:
-        remote_cursor.execute(sql.SQL(f"SELECT * FROM bluerock_plc_values ORDER BY plctime limit " + str(limit) + " offset " + str(offset) + ";"))
+        remote_cursor.execute(sql.SQL(f"SELECT * FROM " + remote_db_name + " ORDER BY plctime limit " + str(limit) + " offset " + str(offset) + ";"))
         items = list(remote_cursor.fetchall())
         if len(items) == 0:
             break
-        s = "%s, " * 59 + "%s"
+        s = "%s, " * (db_row_count - 1) + "%s"
+        print(s)
         print("Data received, inserting")
-        local_cursor.executemany("INSERT INTO bluerock_plc_values values (" + s + ")", items)
+        local_cursor.executemany("INSERT INTO " + local_db_name + " values (" + s + ")", items)
         offset += limit
         print(f"{offset} rows written")
 
-
-
-    # for result in items: 
-    #     print(result)
-
-    # print(type(items[0]))
-
-    # args = ','.join(remote_cursor.mogrify("(%s,%s,%s)", i).decode('utf-8')
-    #             for i in items[0])
-
-    # local_cursor.executemany("INSERT INTO bluerock_plc_values VALUES " + (args))
-    
-
-    # remote_cursor.execute(sql.SQL(f"SELECT count(*) FROM bluerock_plc_values;"))
-    # print(remote_cursor.fetchall())
-
     remote_conn.close()
+
+class CleanArray:
+    def __init__(self, cursor, dbname):
+        self.cursor = cursor
+        self.dbname = dbname
+        self.sensors_to_clip = [ 
+            "feedpressure", 
+            "permeatepressure",
+            "feedtds",
+            "permtds"
+        ]
+
+        self.ro_states = {
+            0: "off",
+            1: "stopped",
+            2: "production",
+            3: "standby",
+            4: "feed_flush",
+            5: "permeate_flush",
+            8: "permeate_flush"
+        }
+
+        self.cached_values = {
+            "feedtds": 0,
+            "permtds": 0
+        }
+
+        self.i = 0
+
+
+    def clip_value_at_zero(self, plctime_str, value_name):
+        self.cursor.execute(sql.SQL(f"SELECT {value_name} FROM {self.dbname} where plctime = '{plctime_str}' limit 1;"))
+        value = list(self.cursor.fetchall())[0][0]
+        new_value = max(0, value)
+        self.cursor.execute(sql.SQL(f"UPDATE {self.dbname} SET {value_name} = {new_value} WHERE plctime = '{plctime_str}';"))
+
+    def cache_if_flusing_or_restore_cache_if_standby(self, plctime_str, value_name):
+        self.cursor.execute(sql.SQL(f"SELECT state FROM {self.dbname} where plctime = '{plctime_str}' limit 1;"))
+        ro_state = list(self.cursor.fetchall())[0][0]
+        ro_state_string = self.ro_states[ro_state]
+        if "flush" in ro_state_string:
+            self.cursor.execute(sql.SQL(f"SELECT {value_name} FROM {self.dbname} where plctime = '{plctime_str}' limit 1;"))
+            sensor_value = list(self.cursor.fetchall())[0][0]
+            self.cached_values[value_name] = sensor_value
+        elif ro_state_string == "standby":
+            self.cursor.execute(sql.SQL(f"UPDATE {self.dbname} SET {value_name} = {self.cached_values[value_name]} WHERE plctime = '{plctime_str}';"))
+            
+
+    def clean_array(self, plctime_str):
+        try :
+            for sensor in self.sensors_to_clip:
+                self.clip_value_at_zero(plctime_str, sensor)
+            
+            for sensor in self.cached_values.keys():
+                self.cache_if_flusing_or_restore_cache_if_standby(plctime_str, sensor)
+        except:
+            raise Exception('no more values')
+
+def clean_data(
+    local_db_name,
+):
+    local_conn = psycopg2.connect(database="waterexp")
+    local_conn.autocommit = True 
+    local_cursor = local_conn.cursor()
+    
+    def get_first_plctime():
+        local_cursor.execute(sql.SQL(f"SELECT plctime FROM {local_db_name} ORDER BY plctime asc limit 1;"))
+        times = list(local_cursor.fetchall())
+        return times[0][0]
+    
+    def datetime_to_string(timestamp):
+        return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+    current_time = get_first_plctime()
+
+    def get_next_time(timestamp):
+        local_cursor.execute(sql.SQL(f"SELECT plctime FROM {local_db_name} where plctime > '{datetime_to_string(timestamp)}' ORDER BY plctime asc limit 1;"))
+        times = list(local_cursor.fetchall())
+        return times[0][0]
+    
+    clean_array = CleanArray(local_cursor, local_db_name)
+
+    while True:
+        try:
+            clean_array.clean_array(datetime_to_string(current_time))
+            current_time = get_next_time(current_time)
+        except:
+            break
+
+    local_conn.close()
+
+
+clean_data("santa_teresa_plc_values")
+
 
 # main()
 # create_plc_tables()
-remote_connect()
+
+# remote_connect(
+#     "santa_teresa_plc_values",
+#     "santa_teresa_plc_values",
+#     58
+# )`
+
+
+
+# psql -d waterexp -c "\copy pryor_farms_plc_values FROM './out2.csv' WITH (FORMAT csv, HEADER true)"
+# psql -h postgres.svwaternet.org -d tsdb -U read_only -c "COPY (SELECT * FROM santa_teresa_plc_values) TO STDOUT WITH CSV HEADER" > out4.csv
+# psql -h postgres.svwaternet.org -d tsdb -U read_only -c "COPY (SELECT * FROM santa_teresa_plc_values) TO STDOUT WITH CSV HEADER" > out4.csv
+# psql -d waterexp -c "\copy santa_teresa_plc_values FROM './out4.csv' WITH (FORMAT csv, HEADER true)"
--- create_plc_table_commands.py
+++ create_plc_table_commands.py
@@ -1,4 +1,137 @@
+createPryorFarmsFull = '''
+--
+-- PostgreSQL database dump
+--
 
+-- Dumped from database version 14.13 (Ubuntu 14.13-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.13 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: pryor_farm_plc_values; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.pryor_farm_plc_values (
+    location text NOT NULL,
+    totalroflow double precision,
+    totalinletflow double precision,
+    totalconcflow double precision,
+    totaldelflow double precision,
+    dumpproduct boolean,
+    wellpumprun boolean,
+    wellpumpauto boolean,
+    feedpumprun boolean,
+    ropumprun boolean,
+    deliveryrun boolean,
+    deliveryauto boolean,
+    inletrun boolean,
+    flushrun boolean,
+    concbypassrun boolean,
+    proddiversionrun boolean,
+    flushdiversionrun boolean,
+    plctime timestamp with time zone NOT NULL,
+    permeateflow double precision,
+    deliveryflow double precision,
+    inletflow double precision,
+    concentrateflow double precision,
+    recycleflow double precision,
+    feedtanklevel double precision,
+    dailypermflow double precision,
+    dailyinletflow double precision,
+    alarm boolean,
+    alarmword integer,
+    rostandby boolean,
+    state integer,
+    lockout boolean,
+    runflush boolean,
+    warnword0 integer,
+    warnword1 integer,
+    totalhrs double precision,
+    permtds double precision,
+    feedtds double precision,
+    permnitrate double precision,
+    permtemp double precision,
+    prodtanklevel double precision,
+    prodtankdisable boolean,
+    prodtankdepth double precision,
+    feedtankdepth double precision,
+    flushtanklevel double precision,
+    flushtankdepth double precision,
+    flushtankfull boolean,
+    inletpressure double precision,
+    concentratepressure double precision,
+    permeatepressure double precision,
+    ropressure double precision,
+    deliverypressure double precision,
+    feedpressure double precision,
+    recyclevalveposition double precision,
+    ropressctrlvalveposition double precision,
+    ropumpspeed double precision,
+    powermeter double precision,
+    flushduret double precision,
+    producttds double precision,
+    recordtime timestamp with time zone DEFAULT now() NOT NULL
+);
+
+
+ALTER TABLE public.pryor_farm_plc_values OWNER TO postgres;
+
+--
+-- Data for Name: pryor_farm_plc_values; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.pryor_farm_plc_values (location, totalroflow, totalinletflow, totalconcflow, totaldelflow, dumpproduct, wellpumprun, wellpumpauto, feedpumprun, ropumprun, deliveryrun, deliveryauto, inletrun, flushrun, concbypassrun, proddiversionrun, flushdiversionrun, plctime, permeateflow, deliveryflow, inletflow, concentrateflow, recycleflow, feedtanklevel, dailypermflow, dailyinletflow, alarm, alarmword, rostandby, state, lockout, runflush, warnword0, warnword1, totalhrs, permtds, feedtds, permnitrate, permtemp, prodtanklevel, prodtankdisable, prodtankdepth, feedtankdepth, flushtanklevel, flushtankdepth, flushtankfull, inletpressure, concentratepressure, permeatepressure, ropressure, deliverypressure, feedpressure, recyclevalveposition, ropressctrlvalveposition, ropumpspeed, powermeter, flushduret, producttds, recordtime) FROM stdin;
+\.
+
+
+--
+-- Name: pryor_farm_plc_values pryor_farm_plc_values_pkey1; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.pryor_farm_plc_values
+    ADD CONSTRAINT pryor_farm_plc_values_pkey1 PRIMARY KEY (location, plctime);
+
+
+--
+-- Name: pryor_farm_plc_values_plctime_idx; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX pryor_farm_plc_values_plctime_idx ON public.pryor_farm_plc_values USING btree (plctime DESC);
+
+
+--
+-- Name: pryor_farm_plc_values ts_insert_blocker; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER ts_insert_blocker BEFORE INSERT ON public.pryor_farm_plc_values FOR EACH ROW EXECUTE FUNCTION _timescaledb_internal.insert_blocker();
+
+
+--
+-- Name: TABLE pryor_farm_plc_values; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT SELECT ON TABLE public.pryor_farm_plc_values TO read_only;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+'''
 
 createSantaTeresaFull = '''
 --
@@ -314,7 +447,7 @@ def createBluerock(system_name):
         flushrun boolean
     );
 
-    CREATE INDEX idx_time ON {system_name} (plctime);
+    CREATE INDEX idx_time_{system_name} ON {system_name} (plctime);
     '''
 
 
@@ -381,5 +514,73 @@ def createSantaTeresa(system_name):
         producttds double precision
     );
 
-    CREATE INDEX idx_time ON {system_name} (plctime);
+    CREATE INDEX idx_time_{system_name} ON {system_name} (plctime);
+    '''
+
+
+def createPryorFarms(system_name):
+    return f'''
+    CREATE TABLE {system_name} (
+        location text NOT NULL,
+        totalroflow double precision,
+        totalinletflow double precision,
+        totalconcflow double precision,
+        totaldelflow double precision,
+        dumpproduct boolean,
+        wellpumprun boolean,
+        wellpumpauto boolean,
+        feedpumprun boolean,
+        ropumprun boolean,
+        deliveryrun boolean,
+        deliveryauto boolean,
+        inletrun boolean,
+        flushrun boolean,
+        concbypassrun boolean,
+        proddiversionrun boolean,
+        flushdiversionrun boolean,
+        plctime timestamp with time zone NOT NULL,
+        permeateflow double precision,
+        deliveryflow double precision,
+        inletflow double precision,
+        concentrateflow double precision,
+        recycleflow double precision,
+        feedtanklevel double precision,
+        dailypermflow double precision,
+        dailyinletflow double precision,
+        alarm boolean,
+        alarmword integer,
+        rostandby boolean,
+        state integer,
+        lockout boolean,
+        runflush boolean,
+        warnword0 integer,
+        warnword1 integer,
+        totalhrs double precision,
+        permtds double precision,
+        feedtds double precision,
+        permnitrate double precision,
+        permtemp double precision,
+        prodtanklevel double precision,
+        prodtankdisable boolean,
+        prodtankdepth double precision,
+        feedtankdepth double precision,
+        flushtanklevel double precision,
+        flushtankdepth double precision,
+        flushtankfull boolean,
+        inletpressure double precision,
+        concentratepressure double precision,
+        permeatepressure double precision,
+        ropressure double precision,
+        deliverypressure double precision,
+        feedpressure double precision,
+        recyclevalveposition double precision,
+        ropressctrlvalveposition double precision,
+        ropumpspeed double precision,
+        powermeter double precision,
+        flushduret double precision,
+        producttds double precision,
+        recordtime timestamp with time zone DEFAULT now() NOT NULL
+    );
+
+    CREATE INDEX idx_time_{system_name} ON {system_name} (plctime);
     '''
